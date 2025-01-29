557d1224b378b641507a450494062db2e6ed96bb
FUKYOURSELF <127204243+FUKYOURSELF@users.noreply.github.com>
2025-01-29 21:49:21 +0800

    Delete test_app directory

--- test_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import function_view_test,ClassViewTest,book_query,book_query1
-urlpatterns=[
-    path('test-function/',function_view_test),
-    path('test-class/',ClassViewTest.as_view()),
-    path('book',book_query),
-    path('book/<str:book_id>',book_query1)
-]
